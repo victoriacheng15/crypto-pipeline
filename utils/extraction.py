8bea3c8b9856cd7060c0fe95936c8b1f234c0f16
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-04-16 12:14:58 -0600

    update source data (#10)
    * chore: switch to new source of data and add to_dict func
    
    * feat: add actions of insert data and delete
    
    * style: the code has been formatted

--- utils/extraction.py
+++ utils/extraction.py
@@ -1,9 +1,44 @@
-import requests, re
-from bs4 import BeautifulSoup
+import requests, os
+from datetime import datetime, timezone
+from dotenv import load_dotenv
 
-def get_data():
-  URL = "https://www.coindesk.com/data/"
-  page = requests.get(URL).text
-  soup = BeautifulSoup(page, "html.parser")
+load_dotenv()
+API_KEY = os.environ.get("CG_API_KEY")
 
-  return soup.find_all(class_=re.compile(r'price-liststyles__ListCardWrapper-sc-ouhin1-2'))
+
+def get_coin_data(total_pages=2):
+    all_data = []
+
+    page = 1
+    while page <= total_pages:
+        query = f"?vs_currency=cad&per_page=250&page={page}"
+        URL = f"https://api.coingecko.com/api/v3/coins/markets{query}"
+        headers = {"accept": "application/json", "x-cg-demo-api-key": API_KEY}
+
+        response = requests.get(URL, headers=headers)
+
+        if response.status_code == 200:
+            all_data.extend(response.json())
+            page += 1
+        else:
+            return None
+
+    return all_data
+
+
+def to_dict(data):
+    return {
+        "name": data["name"],
+        "symbol": data["symbol"],
+        "current_price": data["current_price"],
+        "market_cap": data["market_cap"],
+        "total_supply": data["total_supply"],
+        "high_24h": data["high_24h"],
+        "low_24h": data["low_24h"],
+        "ath": data["ath"],
+        "ath_date": data["ath_date"],
+        "atl": data["atl"],
+        "atl_date": data["atl_date"],
+        "last_updated": data["last_updated"],
+        "save_timestamp": datetime.now(timezone.utc),
+    }
