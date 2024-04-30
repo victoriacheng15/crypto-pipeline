ad6859ba4f00bb950fc1ae5ce7c4c7a9619ea60d
Victoria Cheng <victoria.cheng15@gmail.com>
2024-04-30 15:03:15 -0600

    refactor: update table columns

--- init.sql
+++ init.sql
@@ -1,7 +1,16 @@
 CREATE TABLE IF NOT EXISTS raw_data (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    code VARCHAR(100) NOT NULL,
-    price NUMERIC NOT NULL,
-    extracted_date TIMESTAMP NOT NULL
+    symbol VARCHAR(10) NOT NULL,
+    current_price NUMERIC NOT NULL,
+    market_cap NUMERIC,
+    total_supply NUMERIC,
+    high_24h NUMERIC,
+    low_24h NUMERIC,
+    ath NUMERIC,
+    ath_date TIMESTAMP,
+    atl NUMERIC,
+    atl_date TIMESTAMP,
+    last_updated TIMESTAMP NOT NULL,
+    save_timestamp TIMESTAMP NOT NULL
 );
